450c5696be8bbf3ea37b62a65dca69ef3e843c4a
ajsam123 <ajosesamuel2007@gmail.com>
2025-06-13 23:59:37 +0100

    started working on dashboard contents

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,14 +1,45 @@
-import { Header } from "components";
-import React from "react";
+import { Header, StatsCard, TripCard } from "components";
 
 const dashboard = () => {
   const user = { name: "Samuel" };
+  const dashboardStats = {
+    totalUsers: 12450,
+    userJoined: { currentMonth: 218, lastMonth: 176 },
+    totalTrips: 3210,
+    tripsCreated: { currentMonth: 150, lastMonth: 250 },
+    userRole: { total: 62, currentMonth: 25, lastMonth: 250 },
+  };
+  const { totalTrips, totalUsers, tripsCreated, userJoined, userRole } =
+    dashboardStats;
   return (
     <main className="dashboard wrapper">
       <Header
         title={`Welcome ${user?.name ?? "Guest"}`}
         description="Track activity, trends and popular destinations in real time"
       />
+      <section className="flex flex-col gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full">
+          <StatsCard
+            headerTitle="Total Users"
+            total={totalUsers}
+            currentMonthCount={userJoined.currentMonth}
+            lastMonthCount={userJoined.currentMonth}
+          />
+          <StatsCard
+            headerTitle="Total Users"
+            total={totalTrips}
+            currentMonthCount={tripsCreated.currentMonth}
+            lastMonthCount={tripsCreated.currentMonth}
+          />
+          <StatsCard
+            headerTitle="Active Users"
+            total={userRole}
+            currentMonthCount={userRole.currentMonth}
+            lastMonthCount={userRole.currentMonth}
+          />
+        </div>
+      </section>
+      <TripCard />
     </main>
   );
 };
---
+++ components/StatsCard.tsx
@@ -0,0 +1,21 @@
+import { calculateTrendPercentage } from "lib/utils";
+
+const StatsCard = ({
+  headerTitle,
+  total,
+  lastMonthCount,
+  currentMonthCount,
+}: StatsCard) => {
+  const { trend, percentage } = calculateTrendPercentage(
+    currentMonthCount,
+    lastMonthCount
+  );
+  const isDecrement = trend === "decrement";
+  return (
+    <article className="stats-card">
+      <h3 className="text-base font-medium">{headerTitle}</h3>
+    </article>
+  );
+};
+
+export default StatsCard;
