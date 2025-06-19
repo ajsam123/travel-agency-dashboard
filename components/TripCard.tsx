ddcd09bb63140ec6857514efd5b1bb4b40744f6a
ajsam123 <ajosesamuel2007@gmail.com>
2025-06-19 16:26:49 +0100

    chore: started working on auth

--- components/TripCard.tsx
+++ components/TripCard.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { useLocation } from "react-router";
 import { Link } from "react-router";
+import {
+  ChipListComponent,
+  ChipDirective,
+  ChipsDirective,
+} from "@syncfusion/ej2-react-buttons";
+
+import { cn, getFirstWord } from "lib/utils";
 
 const TripCard = ({
   id,
@@ -33,7 +40,24 @@ const TripCard = ({
           </figure>
           <figcaption>{location}</figcaption>
         </article>
-        <div className="mt-5 pl-[18px] pr-3.5 pb-5"></div>
+        <div className="mt-5 pl-[18px] pr-3.5 pb-5">
+          <ChipListComponent>
+            <ChipsDirective>
+              {tags.map((tag, index) => (
+                <ChipDirective
+                  key={index}
+                  text={getFirstWord(tag)}
+                  cssClass={cn(
+                    index === 1
+                      ? "!bg-pink-50 !text-pink-500"
+                      : "!bg-success-50 !text-success-700"
+                  )}
+                />
+              ))}
+            </ChipsDirective>
+          </ChipListComponent>
+        </div>
+        <article className="tripCard-pill">{price}</article>
       </Link>
     </div>
   );
