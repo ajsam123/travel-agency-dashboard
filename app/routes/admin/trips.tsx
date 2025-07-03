c73d2fa310e37480e723ffc81ce7cf5c6da98c66
ajsam123 <ajosesamuel2007@gmail.com>
2025-07-03 05:40:05 +0100

    chore: working on create trips

--- app/routes/admin/trips.tsx
+++ app/routes/admin/trips.tsx
@@ -1,7 +1,19 @@
+import { Header } from "components";
 import React from "react";
 
 const Trips = () => {
-  return <div>Trips</div>;
+  return (
+    <div>
+      <main className="all-users wrapper">
+        <Header
+          title="Trips"
+          description="View and edit AI-generated travel plans"
+          ctaText="Create a trip"
+          ctaUrl="/trips/create"
+        />
+      </main>
+    </div>
+  );
 };
 
 export default Trips;
